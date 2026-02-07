9e2ebd600791bc109ec1c8d48cb2650a4f5e17fe
Bl4ver <bazsopatrik07@gmail.com>
2026-02-07 13:37:12 +0100

    szar

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ import { enableTemplate } from "./overlay.js";
 
 function ChangeLanguage(btLang){
     for (const e in JSONfile) {
-        document.getElementById(e).innerHTML = JSONfile[e][btLang.value.toLowerCase()];
+        document.getElementById(e).innerHTML = JSONfile[e][btLang.value];
     }
 }
+
+window.ChangeLanguage = ChangeLanguage;
+window.CurrentLanguage = "HU";
 
 document.addEventListener("DOMContentLoaded", async() => {
     window.enableTemplate("settings");
--- js/overlay.js
+++ js/overlay.js
@@ -2,6 +2,7 @@ export function enableTemplate(selected){
     const templateSelected = document.getElementById(`overlay-${selected}`);
     const screen = document.getElementById("screen");
     screen.innerHTML = templateSelected.innerHTML;
+    ChangeLanguage(window.CurrentLanguage);
 }
 
 window.enableTemplate = enableTemplate;
