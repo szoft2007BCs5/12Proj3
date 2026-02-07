4e16a4128ab832f0d785e295d0e5bccae533e7ae
Bl4ver <bazsopatrik07@gmail.com>
2026-02-07 13:42:28 +0100

    save

--- js/main.js
+++ js/main.js
@@ -2,15 +2,12 @@ import { enableTemplate } from "./overlay.js";
 
 function ChangeLanguage(btLang){
     for (const e in JSONfile) {
-        document.getElementById(e).innerHTML = JSONfile[e][btLang.value];
+        document.getElementById(e).innerHTML = JSONfile[e][btLang.value].toLowerCase();
     }
 }
 
-window.ChangeLanguage = ChangeLanguage;
-window.CurrentLanguage = "HU";
-
 document.addEventListener("DOMContentLoaded", async() => {
-    window.enableTemplate("settings");
+    window.enableTemplate("main");
     window.JSONfile = await fetch("../source/data/lang.json")
         .then((promise) => promise.json())
         .catch((error) => console.log(error));
--- js/overlay.js
+++ js/overlay.js
@@ -2,7 +2,6 @@ export function enableTemplate(selected){
     const templateSelected = document.getElementById(`overlay-${selected}`);
     const screen = document.getElementById("screen");
     screen.innerHTML = templateSelected.innerHTML;
-    ChangeLanguage(window.CurrentLanguage);
 }
 
 window.enableTemplate = enableTemplate;
