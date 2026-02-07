b384bb1076649b154f71acddcc51cd8a80cc7970
Bl4ver <bazsopatrik07@gmail.com>
2026-02-07 11:43:54 +0100

    Buttons

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-function ChangeLanguage(btLang){
+function ChangeLanguage(btLang) {
     for (const e in JSONfile) {
         document.getElementById(e).innerHTML = JSONfile[e][btLang.value.toLowerCase()];
     }
 }
 
-document.addEventListener("DOMContentLoaded", async() => {
+function toggleScreen(screen, bt) {
+    const overlay = document.querySelector(`#overlay-${screen}`);
+    if (overlay.style.display === "none") {
+        bt.closest(".overlay").style.display = "none";
+        overlay.style.display = "flex";
+    } else {
+        overlay.style.display = "none";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
     window.JSONfile = await fetch("../source/data/data.json")
         .then((promise) => promise.json())
         .catch((error) => console.log(error));
