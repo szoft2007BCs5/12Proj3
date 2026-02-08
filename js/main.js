ba90bdbd1f3b0e0743e8bd25e1ce3c37558b17de
Bl4ver <bazsopatrik07@gmail.com>
2026-02-08 11:17:22 +0100

    Squashed commit of the following:
    commit 40f055265fd8577f04602e9f570921e2916ee7d8
    Author: Bl4ver <bazsopatrik07@gmail.com>
    Date:   Sat Feb 7 22:43:13 2026 +0100
    
        Button click sound teszt
    
    commit e60ff58f9b5986e9933fa0f5d7ba808e1081b97e
    Author: Bl4ver <bazsopatrik07@gmail.com>
    Date:   Sat Feb 7 21:00:51 2026 +0100
    
        Audio
    
    commit 61fbd5d43ca156ce44e1c0ad57c63c683745c2fa
    Author: Bl4ver <bazsopatrik07@gmail.com>
    Date:   Sat Feb 7 20:59:34 2026 +0100
    
        Music
    
    commit b63fdd2300468ba30eccb2f4258b61ae32940ada
    Author: Bl4ver <bazsopatrik07@gmail.com>
    Date:   Sat Feb 7 18:53:29 2026 +0100
    
        Music
    
    commit 3d534655bcadb842291d6b42f3bc22fd0d3c3a67
    Author: Koviricsi <kovi.richard@hbsz.edu.hu>
    Date:   Sat Feb 7 16:54:47 2026 +0100
    
        0207
    
    commit 19d8a5aa03f08e287b4fbb8bf2d082d5501be030
    Merge: a54c326 115e9cf
    Author: Koviricsi <kovi.richard@hbsz.edu.hu>
    Date:   Sat Feb 7 16:42:14 2026 +0100
    
        Merge branch 'main' of https://github.com/szoft2007BCs5/12Proj3
    
    commit 115e9cfdf6ecfc51a2b6f0f05c9708285bf4ee8f
    Author: Bl4ver <bazsopatrik07@gmail.com>
    Date:   Sat Feb 7 16:39:09 2026 +0100
    
        Hangero
    
    commit a54c326e247b07cd3b1274f176ded66a46fb89c3
    Author: Koviricsi <kovi.richard@hbsz.edu.hu>
    Date:   Sat Feb 7 16:38:39 2026 +0100
    
        vsdg
    
    commit 5fad0fb09c055fa9e17d7a258c2c2258a642e776
    Author: Koviricsi <kovi.richard@hbsz.edu.hu>
    Date:   Sat Feb 7 15:34:20 2026 +0100
    
        Update main.js
    
    commit 802d2a1556b30a4744f13e76035f26277d66db02
    Author: Koviricsi <kovi.richard@hbsz.edu.hu>
    Date:   Sat Feb 7 15:29:48 2026 +0100
    
        fooldal
    
    commit 0b7046e942099637bc7c28e15103a67b49779195
    Author: Bl4ver <bazsopatrik07@gmail.com>
    Date:   Sat Feb 7 14:10:45 2026 +0100
    
        lang
    
    commit 4e16a4128ab832f0d785e295d0e5bccae533e7ae
    Author: Bl4ver <bazsopatrik07@gmail.com>
    Date:   Sat Feb 7 13:42:28 2026 +0100
    
        save
    
    commit 9e2ebd600791bc109ec1c8d48cb2650a4f5e17fe
    Author: Bl4ver <bazsopatrik07@gmail.com>
    Date:   Sat Feb 7 13:37:12 2026 +0100
    
        szar
    
    commit 3726ecab71fe6523c9cbefffef81a3a6689189e2
    Author: Bl4ver <bazsopatrik07@gmail.com>
    Date:   Sat Feb 7 13:14:19 2026 +0100
    
        ye
    
    commit 04e63996caa6670801b2be01ae2361fba171e480
    Author: Bl4ver <bazsopatrik07@gmail.com>
    Date:   Sat Feb 7 11:56:58 2026 +0100
    
        v
    
    commit 9c41e3269fb91745b8dedbe16800e99fed8c5635
    Author: Koviricsi <kovi.richard@hbsz.edu.hu>
    Date:   Sat Feb 7 11:46:52 2026 +0100
    
        e54r632t
    
    commit 7e7254e016f922daced7227a8bf6b0242c5976b4
    Author: Koviricsi <kovi.richard@hbsz.edu.hu>
    Date:   Sat Feb 7 11:07:49 2026 +0100
    
        asd

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-function ChangeLanguage(btLang) {
-    for (const e in JSONfile) {
-        document.getElementById(e).innerHTML = JSONfile[e][btLang.value.toLowerCase()];
-    }
-}
+import * as Overlay from "./overlay.js";
+import * as Audio from "./audio.js";
 
-function toggleScreen(screen, bt) {
-    const overlay = document.querySelector(`#overlay-${screen}`);
-    if (overlay.style.display === "none") {
-        bt.closest(".overlay").style.display = "none";
-        overlay.style.display = "flex";
-    } else {
-        overlay.style.display = "none";
-    }
-}
 
 document.addEventListener("DOMContentLoaded", async () => {
-    window.JSONfile = await fetch("../source/data/data.json")
+    if (!localStorage.getItem("currentLang")) //ha nincs elmentve aktuális nyelv, akkor magyar lesz az alapértelmezett
+        localStorage.setItem("currentLang", "Magyar");
+
+    window.lang = await fetch("../source/data/lang.json") //nyelveket tartalmzó fájl betöltése
         .then((promise) => promise.json())
         .catch((error) => console.log(error));
+
+    Overlay.enableTemplate("main"); //main betöltése
+    Overlay.AddBtEventListeners(); //gombok eseménykezelői
+
+//    Audio.setupButtonClickSounds("../source/audio/button-click4.mp3"); //button click sound beállítása
+
+    document.addEventListener("click", () => {
+        Audio.playAudio("../source/audio/chill-drum-loop.mp3", "music", true); //zene elindítása a mentett hangerővel
+    }, { once: true }); //csak egyszer, hogy ne induljon újra a zene minden kattintásra
 });
